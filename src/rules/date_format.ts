437f1dcd31945c34ad02c24db76475af1f0e7d5c
Erik Isidore <ericssonico@protonmail.ch>
2018-11-14 14:08:49 -0200

    Do stuff

--- src/rules/date_format.ts
+++ src/rules/date_format.ts
@@ -1,7 +1,24 @@
 import RuleContainer from '../core/ruleContainer'
 
+/**
+ * Validate if the input value produces a valid date and optionally if
+ * it follows a given date format.
+ *
+ * @param {String | Number} value - The value of the input to be validated,
+ * if `value` is a String, the validator may also receive a format param to
+ * check if the string has the correct format.
+ * @param {String} format? - Optional parameter to validate whether the the
+ * given input value has the correct format. If given, the value must be a
+ * string, otherwise this parameter is completely ignored.
+ *
+ * @returns {boolean} True if the given value is valid, false otherwise.
+ *
+ * @author Erik Isidore
+ * @version 0.1
+ */
+
 const rule = {
-  validate: (value: any) => {
+  validate: (value: string | number, format?: string): boolean => {
     console.log('date_format')
     return true
   },
--- src/rules/required.ts
+++ src/rules/required.ts
@@ -1,7 +1,18 @@
 import RuleContainer from '../core/ruleContainer'
 
+/**
+ * Validate if the given value is not empty
+ *
+ * @param {Any} value - The value of the input to be validated.
+ *
+ * @returns {boolean} True if the given value is not empty, false otherwise.
+ *
+ * @author Erik Isidore, Viniazvd
+ * @version 0.1
+ */
+
 const rule = {
-  validate: (value: any) => {
+  validate: (value: any): boolean => {
     if (Array.isArray(value)) return !!value.length
     if (typeof value === 'object') return !!Object.keys(value).length
     if (typeof value === 'string') return !!value.trim().length
