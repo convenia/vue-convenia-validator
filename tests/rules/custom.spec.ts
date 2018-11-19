734b55071a557fed778304a9035cd35e78505d55
Erik Isidore <ericssonico@protonmail.ch>
2018-11-19 15:47:06 -0200

    Implement unit tests for validation rules

---
+++ tests/rules/custom.spec.ts
@@ -0,0 +1,23 @@
+import custom from '../../src/rules/custom'
+
+describe('Rule: custom', () => {
+  test('Custom single callback', () => {
+    const callbackOne = (v: number) => v > 0
+    const callbackTwo = (v: string) => !!v.length
+
+    expect(custom.validate(10, callbackOne)).toBe(true);
+    expect(custom.validate('foo', callbackTwo)).toBe(true);
+    expect(custom.validate('', callbackTwo)).toBe(false);
+  })
+
+  test('Multiple callbacks', () => {
+    const callbacks =
+      [ (v: string) => v.length > 5
+      , (v: string) => /^\D+$/.test(v)
+      ]
+
+    expect(custom.validate('Biscoito', callbacks)).toBe(true);
+    expect(custom.validate('FooBarr34', callbacks)).toBe(false);
+    expect(custom.validate('Foo', callbacks)).toBe(false);
+  })
+})
