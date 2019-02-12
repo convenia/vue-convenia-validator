e045a6134f8b07302865a0366969441f51873fcf
Erik Isidore <ericssonico@protonmail.ch>
2019-02-12 14:36:12 -0200

    Fix typo in required rule

--- src/rules/required.ts
+++ src/rules/required.ts
@@ -5,7 +5,7 @@ import { ValidationRule } from '../types'
  * Validate if the given value is not empty
  *
  * @param {Any} value - The value of the input to be validated.
- * @returns {boolean} True if the given value is not empty, false otherwise.
+ * @returns {boolean} - True if the given value is not empty, false otherwise.
  *
  * @author Viniazvd, Erik Isidore
  * @version 0.1
